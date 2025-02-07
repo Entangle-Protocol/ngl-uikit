cf81f280970bc7f94e3908dd33bd6edd1951eb0f
Ar Tira <a.tirabian@entangle.fi>
2025-02-07 12:42:21 +0700

    Ver 0.1.6

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.module\.scss$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            },
+          },
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: {
+      type: 'commonjs2',
+    },
+  },
+  externals: {
+    react: 'react',
+    'react-dom': 'react-dom',
+  },
+}; 
